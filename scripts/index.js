14e291e9fe5ddeab53d284e6140f23a70074eedc
Michael Shervashidze <mike.fury315@gmail.com>
2024-07-13 03:16:51 +0700

    Окончательный результатио

--- scripts/index.js
+++ scripts/index.js
@@ -4,22 +4,22 @@ const cardTemplate = document.querySelector("#card-template").content;
 const addButton = document.querySelector(".profile__add-button");
 const cardList = document.querySelector(".places__list");
 // @todo: Функция создания карточки
-function createCard(card, f) {
+function createCard(cardInfo, deleteCard) {
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-    cardElement.querySelector('.card__title').textContent = card.name;
+    cardElement.querySelector('.card__title').textContent = cardInfo.name;
     const cardElementImage = cardElement.querySelector('.card__image');
-    cardElementImage.src = card.link;
-    cardElementImage.alt = 'На картинке изображено место под названием '+ card.name;
-    cardElement.querySelector('.card__delete-button').addEventListener('click', f);
+    cardElementImage.src = cardInfo.link;
+    cardElementImage.alt = 'На картинке изображено место под названием '+ cardInfo.name;
+    cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
     return cardElement;
 }
 // @todo: Функция удаления карточки
-function resetButton(evt) {
+function deleteCard(evt) {
     const eventTarget = evt.target;
     eventTarget.closest('.card').remove();
 }
 // @todo: Вывести карточки на страницу
 
 initialCards.forEach(function (card) {
-    cardList.append(createCard(card, resetButton));
+    cardList.append(createCard(card, deleteCard));
 });
